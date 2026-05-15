fd228b7c9175a4a8f54114824f4792c8b19af81d
Dipanshu Shukla <shuklascientist5@gmail.com>
2026-05-16 03:12:47 +0530

    init

---
+++ src/main/java/com/dipanshushukla/cop_map_gateway_service/CopMapGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dipanshushukla.cop_map_gateway_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopMapGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopMapGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dipanshushukla/cop_map_gateway_service/CopMapGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dipanshushukla.cop_map_gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopMapGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
